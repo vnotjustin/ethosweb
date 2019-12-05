368b45dbd0f97fc8d3eb1768b6578d0fcfbdcecc
Justin <justintrongvo@gmail.com>
2019-12-04 20:29:59 -0500

    no message

--- modules/header.php
+++ modules/header.php
@@ -8,11 +8,11 @@
     <link rel='stylesheet' type='text/css' href='css/index.css'>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
    <link href="https://fonts.googleapis.com/css?family=Mukta+Vaani:400|Open+Sans:600|Raleway" rel="stylesheet">
-<link rel='shortcut icon' type='image/x-icon' href='images/favicon/favicon.ico' />  
+<!-- <link rel='shortcut icon' type='image/x-icon' href='images/favicon/favicon.ico' />  
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/favicon/apple-touch-icon-144x144.png" />
 <link rel="apple-touch-icon-precomposed" sizes="152x152" href="images/favicon/apple-touch-icon-152x152.png" />
 <link rel="icon" type="image/png" href="images/favicon/favicon-32x32.png" sizes="32x32" />
-<link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16" /> -->
 <meta name="application-name" content="&nbsp;"/>
 <meta name="msapplication-TileColor" content="#FFFFFF" />
 <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png" />
